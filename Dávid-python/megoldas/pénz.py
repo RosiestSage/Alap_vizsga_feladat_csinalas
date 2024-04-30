f5774a2558027ec37c87d6dca588eff767bd8ab6
RosiestSage <pali.david@students.jedlik.eu>
2024-04-30 12:00:14 +0200

    1. feladat

---
+++ Dávid-python/megoldas/pénz.py
@@ -0,0 +1,6 @@
+reszveny = int(input("Adja meg mennyi részvényt venne: "))
+print(f'\n{reszveny} db reszvény ára:')
+print(f'OTP:  {18130 * reszveny} Ft')
+print(f'MOL:  {3032 * reszveny} Ft')
+print(f'BIF:  {564 * reszveny} Ft')
+print(f'Opus:  {383.5 * reszveny} Ft')
